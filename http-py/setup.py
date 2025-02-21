55c7245aa47a9ab9be2a4e4af775723fd4a869ee
22388o <22388o@users.noreply.github.com>
2025-02-21 09:05:53 -0300

    Add setup

---
+++ http-py/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="1.0.0-beta",  # Fixed: Replaced '--' with '-'
+    packages=find_packages(),
+    install_requires=[
+        "Flask==3.1.0",    # Fixed: Properly formatted as a list of strings
+        "requests==2.32.3",
+        "user_agent==0.1.10",
+        "openai==1.63.2"
+    ],
+    author="AI & Robotic Labs",  # Fixed: Closed quotation
+    description="HTTP Privacy JS Bindings",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AII-Robotic-Labs/http-privacy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
