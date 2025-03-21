e4f75e357373161b6362f92eba716288bcd0de13
22388o <22388o@users.noreply.github.com>
2025-03-21 06:33:38 -0300

    Fix

--- http-py/setup.py
+++ http-py/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypackage",
+    name="http-privacy",
     version="1.0.0b1",  # Fixed: Replaced '--' with '-'
     packages=find_packages(),
     install_requires=[
         "Flask==3.1.0",    # Fixed: Properly formatted as a list of strings
         "requests==2.32.3",
         "user_agent==0.1.10",
-        "openai==1.68.0"
+        "openai==1.68.0",
+        "boto3==1.37.17"
+
     ],
     author="AI & Robotic Labs",  # Fixed: Closed quotation
     description="HTTP Privacy JS Bindings",
