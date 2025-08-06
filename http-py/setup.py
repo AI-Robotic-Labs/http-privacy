603086c98cb4a2e8589a069f854515779f0b00d5
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-08-06 07:51:30 -0300

    Update setup.py

--- http-py/setup.py
+++ http-py/setup.py
@@ -12,7 +12,7 @@ setup(
         "mcp==1.12.3"
         "qwen-agent==0.0.29",
         "llama-cpp-python==0.3.14",
-        "openai==1.98.0",
+        "openai==1.99.0",
         "boto3==1.40.1",
         "xai-sdk==1.0.1"
     ],
