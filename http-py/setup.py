b4a85765276f09a696b38f27d48c60b5734a4d1e
22388o <22388o@users.noreply.github.com>
2025-08-28 07:48:57 -0300

    release v1.0.7-beta - Moved to Codeberg

--- http-py/setup.py
+++ http-py/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="http-privacy",
-    version="1.0.0",  # Fixed: Replaced '--' with '-'
+    version="1.0.1",  # Fixed: Replaced '--' with '-'
     packages=find_packages(),
     install_requires=[
         "Flask==3.1.2",    # Fixed: Properly formatted as a list of strings
